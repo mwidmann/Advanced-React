7a3934b6c562f0e81955eca7242663a06e37fd1a
Martin Widmann <martin.widmann@russmedia.com>
2018-11-01 16:44:33 +0100

    found method of using theme with vue-styled-components switched from radnika to lato as nuxt doesn't do mimetypes for woff2 well

--- sick-fits/frontend-vue/components/Header.vue
+++ sick-fits/frontend-vue/components/Header.vue
@@ -1,21 +1,67 @@
 <template>
-  <div>
+  <StyledHeader>
     <div class="bar">
-      <a href="">Sick Fits</a>
+      <Logo>
+        <nuxt-link to="/">Sick Fits</nuxt-link>
+      </Logo>
       <Nav></Nav>
     </div>
     <div class="sub-bar">
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 </template>
 
 <script>
+import styled from 'vue-styled-components'
 import Nav from '~/components/Nav'
+import theme from '~/assets/theme'
+
+const Logo = styled.h1`
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    background: ${theme.red};
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`
+
+const StyledHeader = styled.header`
+  .bar {
+    border-bottom: 10px solid ${theme.black};
+    display: grid;
+    justify-content: space-between;
+    grid-template-columns: auto 1fr;
+    align-items: stretch;
+
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-contnet: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 1px solid ${theme.lightgrey};
+  }
+`
+
 export default {
   components: {
-    Nav
+    Nav,
+    StyledHeader,
+    Logo
   }
 }
 </script>
