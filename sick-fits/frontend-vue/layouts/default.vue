116a5f4d48ef0328996b9ff81dd8f1a8147c8387
Martin Widmann <martin.widmann@russmedia.com>
2018-11-02 16:51:35 +0100

    re-added for debugging

--- sick-fits/frontend-vue/layouts/default.vue
+++ sick-fits/frontend-vue/layouts/default.vue
@@ -33,6 +33,13 @@ injectGlobal`
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
 
+  @font-face {
+    font-family: 'radnika_next';
+    src: url('/assets/radnikanext-medium-webfont.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+  }
+
   html {
     box-sizing: border-box;
     font-size: 10px;
@@ -47,7 +54,7 @@ injectGlobal`
     margin: 0;
     font-size: 1.5rem;
     line-height: 2;
-    font-family: 'Lato';
+    font-family: 'radnika_next';
   }
 
   a {
