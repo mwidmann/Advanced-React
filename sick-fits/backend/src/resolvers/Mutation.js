cf6b734e96763fa3fb192f11f0d315203586d148
Martin Widmann <martin.widmann@russmedia.com>
2018-11-14 22:21:09 +0100

    added updating and deletion of items

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -8,12 +8,28 @@ const mutations = {
     return item
   },
   async updateItem(parent, args, ctx, info) {
-    const updatedItem = await ctx.db.mutation.updateItem({
-      data: { ...args }
-    }, {
-      id: args.id
+    // take a copy of the updates
+    const updates = { ...args }
+    // remove the id from the updates
+    delete updates.id
+    // run the update
+    return ctx.db.mutation.updateItem({
+      data: updates,
+      where: {
+        id: args.id
+      }
     }, info)
-    return updatedItem
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{
+      id
+      title
+    }`)
+    // check if they own that item, or have the permission to delete it
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info)
   }
 };
 
