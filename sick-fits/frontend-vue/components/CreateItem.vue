f58b8125483030216a4ce0f7690b35f3bc15d960
Martin Widmann <martin.widmann@russmedia.com>
2018-11-14 13:47:21 +0100

    Now uploading pictures to cloudinary ☁

--- sick-fits/frontend-vue/components/CreateItem.vue
+++ sick-fits/frontend-vue/components/CreateItem.vue
@@ -2,23 +2,29 @@
 <div>
   <apollo-mutation
     :mutation="CREATE_ITEM_MUTATION"
-    :variables="{ title, price, description }"
+    :variables="{ title, price, description, image, largeImage }"
     @done="onDone">
     <template slot-scope="{ mutate, loading, error }">
       <Form @submit.native.prevent="mutate">
         <Error :error="error"></Error>
         <fieldset :disabled="loading" :aria-busy="loading">
-          <label htmlFor="title">
+          <label for="file">
+            Image
+            <input type="file" id="file" name="file" placeholder="Upload an image" @change="uploadFile" />
+          </label>
+          <img width="200" v-if="image" alt="Upload Preview" :src="image"/>
+
+          <label for="title">
             Title
             <input type="text" id="title" name="title" placeholder="Title" required v-model="title" />
           </label>
 
-          <label htmlFor="price">
+          <label for="price">
             Price
             <input type="number" id="price" name="price" placeholder="Price" required v-model="price" />
           </label>
 
-          <label htmlFor="description">
+          <label for="description">
             Description
             <textarea type="number" id="description" name="description" placeholder="Enter a description" required v-model="description" />
           </label>
@@ -76,6 +82,23 @@ export default {
   methods: {
     onDone(result) {
       this.$router.push(`/item/${result.data.createItem.id}`)
+    },
+    async uploadFile(e) {
+      const files = e.target.files
+      const data = new FormData()
+      data.append('file', files[0])
+      data.append('upload_preset', 'sickfits')
+
+      const res = await fetch(
+        'https://api.cloudinary.com/v1_1/dm9gla83n/image/upload',
+        {
+          method: 'POST',
+          body: data
+        }
+      )
+      const file = await res.json()
+      this.image = file.secure_url
+      this.largeImage = file.eager[0].secure_url
     }
   },
   computed: {
--- sick-fits/frontend/components/CreateItem.js
+++ sick-fits/frontend/components/CreateItem.js
@@ -40,7 +40,25 @@ class CreateItem extends Component {
     this.setState({
       [name]: val
     })
+  };
+
+  uploadFile = async e => {
+    const files = e.target.files
+    const data = new FormData();
+    data.append('file', files[0])
+    data.append('upload_preset', 'sickfits')
+
+    const res = await fetch('https://api.cloudinary.com/v1_1/dm9gla83n/image/upload', {
+      method: 'POST',
+      body: data
+    })
+    const file = await res.json()
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    })
   }
+
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
@@ -58,6 +76,11 @@ class CreateItem extends Component {
           }}>
           <Error error={error} />
           <fieldset disabled={loading} aria-busy={loading}>
+            <label htmlFor="file">
+                Image
+              <input type="file" id="file" name="file" placeholder="Upload an image" onChange={this.uploadFile} />
+            </label>
+            {this.state.image && <img width="200" src={this.state.image} alt="Upload Preview" />}
             <label htmlFor="title">
               Title
               <input type="text" id="title" name="title" placeholder="Title" required value={this.state.title} onChange={this.handleChange} />
