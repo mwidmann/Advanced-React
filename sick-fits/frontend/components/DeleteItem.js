723ecb6d093b1625471837d21dba39ad52221d29
Martin Widmann <martin.widmann@russmedia.com>
2018-11-14 22:21:09 +0100

    added updating and deletion of items

---
+++ sick-fits/frontend/components/DeleteItem.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react'
+import { Mutation } from 'react-apollo'
+import gql from 'graphql-tag'
+import { ALL_ITEMS_QUERY } from './Items'
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`
+
+class DeleteItem extends Component {
+  update = (cache, payload) => {
+    // manually update the cache on the client
+    const data = cache.readQuery({ query: ALL_ITEMS_QUERY })
+    // filter the deleted items out of the page
+    data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id)
+    // put the items back
+    cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
+  }
+
+  render() {
+    return (
+      <Mutation
+        mutation={DELETE_ITEM_MUTATION}
+        variables={{ id: this.props.id }}
+        update={this.update}
+      >
+        {(deleteItem, { error }) => (
+          <button onClick={() => {
+            if (confirm('Are you sure?')) {
+              deleteItem()
+            }
+          }}>{this.props.children}</button>
+        )}
+      </Mutation>
+    )
+  }
+}
+
+export default DeleteItem
--- sick-fits/frontend/components/Item.js
+++ sick-fits/frontend/components/Item.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import Title from './styles/Title'
 import ItemStyles from './styles/ItemStyles'
 import PriceTag from './styles/PriceTag'
+import DeleteItem from './DeleteItem'
 import formatMoney from '../lib/formatMoney'
 
 export default class Item extends Component {
@@ -35,7 +36,7 @@ export default class Item extends Component {
             query: { id: item.id }
           }}><a>Edit ✏</a></Link>
         <button>Add To Cart</button>
-        <button>Delete</button>
+        <DeleteItem id={item.id}>Delete this item</DeleteItem>
       </div>
     </ItemStyles>
     )
