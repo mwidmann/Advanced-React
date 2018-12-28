fd0aa2b77999f4783b38a170bc2b901684a7b807
Martin Widmann <martin.widmann@russmedia.com>
2018-12-28 10:14:58 +0100

    Items can be deleted

---
+++ sick-fits/frontend-vue/components/DeleteItem.vue
@@ -0,0 +1,45 @@
+<template>
+  <button @click="deleteItem"><slot></slot></button>
+</template>
+
+<script>
+import gql from 'graphql-tag'
+import { ALL_ITEMS_QUERY } from './Items'
+
+const DELETE_ITEM_MUTATION = gql`
+  mutation DELETE_ITEM_MUTATION($id: ID!) {
+    deleteItem(id: $id) {
+      id
+    }
+  }
+`
+
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    deleteItem() {
+      if (confirm('Are you sure?')) {
+        this.$apollo.mutate({
+          mutation: DELETE_ITEM_MUTATION,
+          variables: {
+            id: this.id,
+          },
+          update(cache, payload) {
+            // manually update the cache on the client
+            const data = cache.readQuery({ query: ALL_ITEMS_QUERY })
+            // filter the deleted items out of the page
+            data.items = data.items.filter(item => item.id !== payload.data.deleteItem.id)
+            // put the items back
+            cache.writeQuery({ query: ALL_ITEMS_QUERY, data })
+          }
+        })
+      }
+    }
+  }
+}
+</script>
