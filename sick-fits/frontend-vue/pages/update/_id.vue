befba2d4191bb2cf71be48d7d393c56c8ee30299
Martin Widmann <martin.widmann@russmedia.com>
2018-12-28 10:14:31 +0100

    Added Item updating capabilities

---
+++ sick-fits/frontend-vue/pages/update/_id.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <update-item :id="$route.params.id"></update-item>
+  </div>
+</template>
+
+<script>
+import UpdateItem from '../../components/UpdateItem'
+export default {
+  components: {
+    UpdateItem
+  }
+}
+</script>
