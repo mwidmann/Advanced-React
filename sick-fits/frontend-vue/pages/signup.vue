da6db459774feec22f9221ae6d0ad30b044447bc
Martin Widmann <martin.widmann@russmedia.com>
2018-12-30 08:23:35 +0100

    added signing up

---
+++ sick-fits/frontend-vue/pages/signup.vue
@@ -0,0 +1,25 @@
+<template>
+  <Columns>
+    <signup></signup>
+    <signup></signup>
+    <signup></signup>
+  </Columns>
+</template>
+
+<script>
+import styled from 'vue-styled-components'
+import Signup from '../components/Signup'
+
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`
+
+export default {
+  components: {
+    Signup,
+    Columns
+  }
+}
+</script>
