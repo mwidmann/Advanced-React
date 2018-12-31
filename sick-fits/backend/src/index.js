f7fb73059f571b00dc1ef194b604942c93504517
Martin Widmann <martin.widmann@russmedia.com>
2018-12-31 09:47:43 +0100

    added backend user authenticatation through jwt

--- sick-fits/backend/src/index.js
+++ sick-fits/backend/src/index.js
@@ -1,19 +1,33 @@
-const cookieParser = require('cookie-parser')
-require('dotenv').config({ path: 'variables.env' })
-const createServer = require('./createServer')
-const db = require('./db')
+const cookieParser = require('cookie-parser');
+require('dotenv').config({ path: 'variables.env' });
+const jwt = require('jsonwebtoken');
+const createServer = require('./createServer');
+const db = require('./db');
 
 const server = createServer();
 
-server.express.use(cookieParser())
-// TODO: use express middleware to populate current user
+server.express.use(cookieParser());
 
-console.log([process.env.FRONTEND_URL, process.env.VUE_FRONTEND_URL])
-server.start({
-  cors: {
-    credentials: true,
-    origin: [process.env.FRONTEND_URL, process.env.VUE_FRONTEND_URL],
+// decode the JWT and add the user-id to each request
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    // put user-id onto the request for further requests
+    req.userId = userId;
   }
-}, deets => {
-  console.log(`Server is now running on http://localhost:${deets.port}`)
-})
+  next();
+});
+
+console.log([process.env.FRONTEND_URL, process.env.VUE_FRONTEND_URL]);
+server.start(
+  {
+    cors: {
+      credentials: true,
+      origin: [process.env.FRONTEND_URL, process.env.VUE_FRONTEND_URL],
+    },
+  },
+  deets => {
+    console.log(`Server is now running on http://localhost:${deets.port}`);
+  }
+);
