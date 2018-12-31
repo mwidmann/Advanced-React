de8c992ef158bdde5a780a9378c358e444d8caa3
Martin Widmann <martin.widmann@russmedia.com>
2018-12-31 09:47:43 +0100

    added backend user authenticatation through jwt

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,61 +1,73 @@
-const bcrypt = require('bcryptjs')
-const jwt = require('jsonwebtoken')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const mutations = {
   async createItem(parent, args, ctx, info) {
     // TODO: Check if they are logged in
 
-    const item = await ctx.db.mutation.createItem({
-      data: { ...args }
-    }, info)
-    return item
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: { ...args },
+      },
+      info
+    );
+    return item;
   },
   async updateItem(parent, args, ctx, info) {
     // take a copy of the updates
-    const updates = { ...args }
+    const updates = { ...args };
     // remove the id from the updates
-    delete updates.id
+    delete updates.id;
     // run the update
-    return ctx.db.mutation.updateItem({
-      data: updates,
-      where: {
-        id: args.id
-      }
-    }, info)
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id,
+        },
+      },
+      info
+    );
   },
   async deleteItem(parent, args, ctx, info) {
-    const where = { id: args.id }
+    const where = { id: args.id };
     // find the item
-    const item = await ctx.db.query.item({ where }, `{
+    const item = await ctx.db.query.item(
+      { where },
+      `{
       id
       title
-    }`)
+    }`
+    );
     // check if they own that item, or have the permission to delete it
     // delete it
-    return ctx.db.mutation.deleteItem({ where }, info)
+    return ctx.db.mutation.deleteItem({ where }, info);
   },
-  //signup(email: String!, password: String!, name: String!): User!
+  // signup(email: String!, password: String!, name: String!): User!
   async signup(parent, args, ctx, info) {
-    args.email = args.email.toLowerCase()
+    args.email = args.email.toLowerCase();
     // hash the password
-    const password = await bcrypt.hash(args.password, 10)
+    const password = await bcrypt.hash(args.password, 10);
     // create the user in the database
-    const user = await ctx.db.mutation.createUser({
-      data: {
-        ...args,
-        password,
-        permissions: { set: ['USER'] }
-      }
-    }, info)
+    const user = await ctx.db.mutation.createUser(
+      {
+        data: {
+          ...args,
+          password,
+          permissions: { set: ['USER'] },
+        },
+      },
+      info
+    );
     // create a jwt token
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET);
     // set jwt as a cookie on the response
     ctx.response.cookie('token', token, {
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24 * 365,
-    })
+    });
     return user;
-  }
+  },
 };
 
 module.exports = mutations;
