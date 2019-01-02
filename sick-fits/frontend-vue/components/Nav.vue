6375fdfa505cb3716c68f4c4ac78afc15bfb0af2
Martin Widmann <martin.widmann@russmedia.com>
2019-01-02 11:06:37 +0100

    implemented singing out

--- sick-fits/frontend-vue/components/Nav.vue
+++ sick-fits/frontend-vue/components/Nav.vue
@@ -7,6 +7,7 @@
             <nuxt-link to="/sell">sell</nuxt-link>
             <nuxt-link to="/me">me</nuxt-link>
             <nuxt-link to="/orders">orders</nuxt-link>
+            <signout></signout>
           </template>
           <nuxt-link to="/signup" v-if="!data || !data.me">Sign in</nuxt-link>
         </nav-styles>
@@ -17,11 +18,13 @@
 <script>
 import NavStyles from './styles/NavStyles'
 import User from './User'
+import Signout from './Signout'
 
 export default {
   components: {
     NavStyles,
-    User
+    User,
+    Signout,
   },
   methods: {
     log(data) {console.log(data)}
---
+++ sick-fits/frontend-vue/components/Signout.vue
@@ -0,0 +1,40 @@
+
+<template>
+  <apollo-mutation
+    :mutation="SIGNOUT_MUTATION"
+    :refetchQueries="refetchQueries"
+    tag=""
+  >
+    <button type="button" @click="mutate" slot-scope="{ mutate }">Sign out</button>
+  </apollo-mutation>
+</template>
+
+<script>
+import { ApolloMutation } from 'vue-apollo'
+import gql from 'graphql-tag'
+import { CURRENT_USER_QUERY } from './User'
+
+const SIGNOUT_MUTATION = gql`
+  mutation SIGNOUT {
+    signout {
+      message
+    }
+  }
+`
+
+export default {
+  components: {
+    ApolloMutation,
+  },
+  methods: {
+    refetchQueries() {
+      return [{ query: CURRENT_USER_QUERY }]
+    },
+  },
+  computed: {
+    SIGNOUT_MUTATION() {
+      return SIGNOUT_MUTATION
+    }
+  }
+}
+</script>
