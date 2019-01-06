8c06a523f35fb60b5b1e43b3342660e2f7b54ea2
Martin Widmann <martin.widmann@russmedia.com>
2019-01-06 11:54:37 +0100

    updating permissions to the database 🔒

--- sick-fits/frontend-vue/plugins/global-components.js
+++ sick-fits/frontend-vue/plugins/global-components.js
@@ -1,7 +1,8 @@
 import Vue from 'vue'
-
+import { Plugin } from 'vue-fragment'
 import Form from '../components/styles/Form'
 import Error from '../components/ErrorMessage'
 
+Vue.use(Plugin)
 Vue.component('Form', Form)
 Vue.component('Error', Error)
