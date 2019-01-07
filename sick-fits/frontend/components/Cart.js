391d005d0619a6c24d3ad2d0ebf22d93ccbb4819
Martin Widmann <martin.widmann@russmedia.com>
2019-01-07 13:32:26 +0100

    added the shopping cart, for now always open 🛒

---
+++ sick-fits/frontend/components/Cart.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import CartStyles from './styles/CartStyles';
+import Supreme from './styles/Supreme';
+import CloseButton from './styles/CloseButton';
+import SickButton from './styles/SickButton';
+
+const Cart = () => (
+  <CartStyles open>
+    <header>
+      <CloseButton title="Close">&times;</CloseButton>
+      <Supreme>Your cart</Supreme>
+      <p>You have __ items in your cart.</p>
+    </header>
+    <footer>
+      <p>$10.10</p>
+      <SickButton type="button">Checkout</SickButton>
+    </footer>
+  </CartStyles>
+);
+
+export default Cart;
