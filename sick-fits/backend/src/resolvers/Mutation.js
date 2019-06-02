0853e7d69355d2d90494be731f40ab44056f6308
Martin Widmann <martin.widmann@russmedia.com>
2019-06-02 12:12:43 +0200

    Items can now be removed from the cart, in a non reactive way

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -264,6 +264,25 @@ const mutations = {
       info
     );
   },
+  async removeFromCart(parent, args, ctx, info) {
+    // 1. Find the CartItem
+    const cartItem = await ctx.db.query.cartItem(
+      {
+        where: {
+          id: args.id,
+        },
+      },
+      `{ id, user { id }}`
+    );
+    // 1.5 make sure we found an item
+    if (!cartItem) throw new Error('No CartItem found!');
+    // 2. Make sure they own the CartItem
+    if (cartItem.user.id !== ctx.request.userId) {
+      throw new Error('Cheatin, huh?');
+    }
+    // 3. Delete the CartItem
+    return ctx.db.mutation.deleteCartItem({ where: { id: args.id } }, info);
+  },
 };
 
 module.exports = mutations;
---
+++ sick-fits/frontend-vue/components/RemoveFromCart.vue
@@ -0,0 +1,46 @@
+<template>
+  <ApolloMutation
+    :mutation="require('~/graphql/mutations/RemoveFromCart.gql')"
+    tag=""
+    :variables="{id}"
+  >
+    <BigButton
+      slot-scope="{ mutate, loading }"
+      :disabled="loading"
+      title="Delete Item"
+      @click="mutate"
+    >
+      &times;
+    </BigButton>
+  </ApolloMutation>
+</template>
+
+<script>
+import styled from 'vue-styled-components'
+import theme from '~/assets/theme'
+
+const BigButton = styled.button`
+  font-style: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: ${theme.red};
+    cursor: pointer;
+  }
+`
+
+export default {
+  components: {
+    BigButton,
+  },
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style>
+</style>
